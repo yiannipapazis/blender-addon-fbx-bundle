66d8d4a0f21c7f16754eb39138d19cd942b31064
Yianni Papazis <yianni.papazis@gmail.com>
2021-01-12 19:53:08 +1030

    fixed curves not exportiing in UE profile

--- objects_organise.py
+++ objects_organise.py
@@ -63,14 +63,14 @@ def get_objects():
 
 			# Collect groups from input selection
 			for obj in objects:
-				for group in obj.users_group:
+				for group in obj.users_collection:
 					if group.name not in groups:
 						groups.append(group.name)
 
 			# Collect objects of groups
 			for name in groups:
-				if name in bpy.data.groups:
-					for obj in bpy.data.groups[name].objects:
+				if name in bpy.data.collections:
+					for obj in bpy.data.collections[name].objects:
 						if obj not in objects:
 							objects.append(obj)
 
